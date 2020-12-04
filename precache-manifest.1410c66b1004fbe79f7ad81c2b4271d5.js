ab371e749f3ac00748920e0c8179ea5bd4cb2d30
VeNoM <kumarsks619@gmail.com>
2020-12-04 17:52:25 +0530

    Updates

---
+++ precache-manifest.1410c66b1004fbe79f7ad81c2b4271d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7066d6865e9d3e712319bb9c17660fa4",
+    "url": "/portfolio-sample/index.html"
+  },
+  {
+    "revision": "947c8186b9b48fff4f91",
+    "url": "/portfolio-sample/static/css/main.37f611b1.chunk.css"
+  },
+  {
+    "revision": "3a10c3f3e985e627315d",
+    "url": "/portfolio-sample/static/js/2.5886da71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-sample/static/js/2.5886da71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "947c8186b9b48fff4f91",
+    "url": "/portfolio-sample/static/js/main.cea1deaf.chunk.js"
+  },
+  {
+    "revision": "e5950342af5eb17708c6",
+    "url": "/portfolio-sample/static/js/runtime-main.0b8107c3.js"
+  },
+  {
+    "revision": "12ee46e4b9d8c834760d884ead6bb9a5",
+    "url": "/portfolio-sample/static/media/uStoreCEO.12ee46e4.png"
+  }
+]);
